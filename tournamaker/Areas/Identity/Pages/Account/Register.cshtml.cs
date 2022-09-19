f52a63f24899c8fe2add7d7c50eaf777ad51a245
TanguyIglesias <tanguyiglesias@gmail.com>
2022-09-19 09:57:02 +0200

    login + register

--- tournamaker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ tournamaker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -127,8 +127,12 @@ namespace tournamaker.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+                user.Name = Input.Name;
+                user.DOB = Input.DOB;
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
