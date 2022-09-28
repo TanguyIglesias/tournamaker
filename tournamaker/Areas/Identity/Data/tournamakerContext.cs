e983cab481cd13e538eb7a10a806aba56f36153d
TanguyIglesias <tanguyiglesias@gmail.com>
2022-09-28 16:36:23 +0200

    Authorize Tournoi

--- tournamaker/Areas/Identity/Data/tournamakerContext.cs
+++ tournamaker/Areas/Identity/Data/tournamakerContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using tournamaker.Areas.Identity.Data;
+using tournamaker.Models;
 
 namespace tournamaker.Data;
 
@@ -19,4 +20,6 @@ public class tournamakerContext : IdentityDbContext<tournamakerUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+    public DbSet<tournamaker.Models.Tournoi> Tournoi { get; set; }
 }
